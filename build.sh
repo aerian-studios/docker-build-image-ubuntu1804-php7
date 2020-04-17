d67a4508d8543fc0beb55cff3563ec983fc23055
joshedney <josh.edney@aerian.com>
2020-04-17 10:08:36 +0100

    add date to docker versions

--- build.sh
+++ build.sh
@@ -10,13 +10,18 @@ set -e
 
 project=$1
 
+now=$(date +"%d-%m-%Y")
+
 owner="aerian-studios-jenkins"
 version="0.0.1"
 
 #docker build --no-cache -t $project .
 docker build -t $project .
 
+echo "Tagging latest image"
 docker tag "$project:latest" "$owner/$project:$version"
+echo "Tagging dated image"
+docker tag "$project:latest" "$owner/$project:$now"
 
 #push the image to github/ECR
 #docker login
